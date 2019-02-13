34cb527f1e5181313d343062f297e3d29d046fc0
jbryslaw <jbryslaw@gmail.com>
2019-02-13 08:30:34 -0500

    Updating Model

--- NLParmor_model.py
+++ NLParmor_model.py
@@ -32,19 +32,12 @@ def NLP_model(b_binary, labels_index, max_length, TOKEN_UNIVERSE, Embed ):
         
     ly_merge = keras.layers.Concatenate(axis=1)(ly_N_conv)
 
-    # ly_final_conv = Conv1D(filters=128, kernel_size=3, activation=st_act)(ly_merge)
-    # ly_final_pool = MaxPooling1D(pool_size=3)(ly_final_pool)
-    # ly_final_drop = Dropout(0.5)(ly_final_drop)
-    #    ly_final_batchN = BatchNormalization()(ly_merge)
     ly_drop = Dropout(0.5)(ly_merge)
     ly_flat = Flatten()(ly_drop)
 
     ly_dense = Dense(128, activation=st_act)(ly_flat)
 
-    if b_binary:
-        ly_out = Dense(labels_index, activation='softmax')(ly_dense)
-    else:
-        ly_out = Dense(labels_index, activation='sigmoid')(ly_dense)
+    ly_out = Dense(labels_index, activation='sigmoid')(ly_dense)
     model = Model(ly_in, ly_out)
     
     return model
